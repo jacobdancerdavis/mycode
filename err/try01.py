3a716d18c6d394f850344aeb029926b4b302ffc7
jacobdancerdavis <jb_davis70@yahoo.com>
2023-08-18 16:00:28 +0000

    A bit on error handling

---
+++ err/try01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+while True:
+    try:
+        print("Enter a file name: ")
+        name = input()
+        with open(name, "w") as myfile:
+            myfile.write("No problems with that file name.")
+        break
+    except:
+        print("Error with thtat file name! Try again...")
+
